f5336aad89fb194a2edf1a725643d756d31cf32d
blorbo <teovg@kth.se>
2026-02-04 20:02:49 +0100

    Trying out basic file operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-    // hello bro
+    #ifdef _WIN32
+    string p = "C:\\";
+    ofstream out("output.txt");
+    #endif
+
+    #ifdef __linux__
+    string p = "/home/";
+    ofstream out("output.txt");
+    #endif
+    //testing fstream
+    out << "This is a test." << endl;
+    out.close();
 
 }
