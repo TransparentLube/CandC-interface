8af448031e0209a2b057fd42425c104baea7bad6
teovg <teovg@kth.se>
2026-02-06 16:01:06 +0100

    ok fixade cargo skiten nu actual :pray:

---
+++ tcp/target/debug/deps/tcp-f496173824149f02.d
@@ -0,0 +1,7 @@
+D:\Repos\CandC-interface\tcp\target\debug\deps\tcp-f496173824149f02.d: src\main.rs src\client.rs src\server.rs
+
+D:\Repos\CandC-interface\tcp\target\debug\deps\libtcp-f496173824149f02.rmeta: src\main.rs src\client.rs src\server.rs
+
+src\main.rs:
+src\client.rs:
+src\server.rs:
